42240d023197b94d0a29058e8715651e0a03d860
Isaqcx1 <isaqc1705@gmail.com>
2025-12-09 03:15:23 -0500

    Delete parcialint directory

--- parcialint/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponente } from './app/app.componente';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { appConfig } from './app/app.config';
-
-bootstrapApplication(AppComponente, {
-  providers: [provideRouter(routes)]
-} );
